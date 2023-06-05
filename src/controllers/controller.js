9004a24709d838f169593d260ec4b06e4207399e
Preeti Bhardwaj <bhardwajpreeti@gmail.com>
2023-06-05 18:44:58 +0530

    preeti/day3

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,8 +1,9 @@
 const shortId = require("shortid");
 const urlModel = require("../model/urlmodel");
 const validUrl=require('valid-url')
-const dotenv = require('dotenv').config() 
-
+const { GET_ASYNC, SET_ASYNC } = require("../utils/redis");
+const axios = require("axios");
+const { response } = require("express");
 
 
 const createUrlShorten = async (req, res) => {
@@ -11,47 +12,73 @@ const createUrlShorten = async (req, res) => {
     data.longUrl = data.longUrl.trim()
 
     // if input is empty
-     if (!data.longUrl)
+     if (!data.longUrl){
        return res.status(400).send({ status: false, message: "Please, Provide URL" });
+     }
 
     // URL is valid and string
     if(!validUrl.isWebUri(data.longUrl)){
       return res.status(400).send({status:false,message:"Your URL is not a valid URL"})
       }
 
+
   //  if long url already exist in DB return the respective shorturl and urlcode for it
     const dbData = await urlModel.findOne({ longUrl: data.longUrl }).select({ _id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 });
 
     if (dbData) {
-      return res.status(201).send({ status: true, data: dbData });
-    }
+      return res.status(200).send({ status: true, data: dbData });
+    }else{
 
-    // if longurl is new, create short urlcode and shorturl for it 
-      let shortCode = shortId.generate()
-     
-      data.urlCode = shortCode;
+    const response= await axios
+    .get(data.longUrl)
+    .then(async (response) => {
+// if longurl is new, create short urlcode and shorturl for it short id convert into the lower case string-----------------------------------------------
+          let shortCode = shortId.generate().toLowerCase();
 
-      data.shortUrl = `http://localhost:${process.env.PORT}/${shortCode}`;
+          const code =await urlModel.findOne({ urlCode: shortCode });
+          if (code) {
+            shortCode = shortId.generate().toLowerCase();;
+          }
 
-      const saveData =await urlModel.create(data);
+          data.urlCode = shortCode;
+        // Handle successful response
+        data.shortUrl = `http://${req.headers.host}/${data.urlCode}`;
+      
+      await urlModel.create(data);
+      const saveData = await urlModel.findOne({longUrl:data.longUrl}).select({ _id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 });
+
       res.status(201).send({ status: true, data: saveData });
-    
-  } catch (error) {
+    })
+    .catch((error) => {
+    // Handle error
+     return res.status(400).json({ status: false, message:"url link in invalid"})
+    });
+  }
+   }catch(error) {
     res.status(500).send({ status: false, message: error.message });
   }
 };
 
 const getUrl = async (req, res) => {
   try {
-    const url = await urlModel.findOne({ urlCode: req.params.urlCode });
-    if (url) {
-      res.status(302).redirect(url.longUrl);
-    } else {
-      res.status(404).send({status: false, message:"Not found"});
+
+    const fetchFromRedis = await GET_ASYNC(`${req.params.urlCode}`);
+    if (fetchFromRedis) {
+      res.status(302).redirect(JSON.parse(fetchFromRedis));
+    }
+    else {
+      const url = await urlModel.findOne({ urlCode: req.params.urlCode });
+      if (url) {
+        await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(url.longUrl),"EX",60 * 60)
+        res.status(302).redirect(url.longUrl);
+      }
+      else {
+        res.status(404).send({ status: false, message: "URL Not found " });
+      }
     }
   } catch (error) {
     res.status(500).send({ status: false, error: error.message });
   }
-}
+};
 
 module.exports = { createUrlShorten, getUrl };
